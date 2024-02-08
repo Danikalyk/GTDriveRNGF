54affe1ecbf380a406aee4c1bde6fd63ec733a59
Belousov <wasik787@gmail.com>
2024-02-08 21:42:45 +0500

    refactor: getRoutes user query

--- src/api/routes.js
+++ src/api/routes.js
@@ -5,8 +5,8 @@ export async function uploadLocation(params) {
   return putRequest('/geo_info_users', params);
 }
 
-export async function getRoutes() {
-  return getRequest('/routes');
+export async function getRoutes(userId) {
+  return getRequest(`/routes?user=${userId}`);
 }
 
 export async function getRoute(uid) {
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -7,18 +7,21 @@ import {
   Text,
   TopNavigation,
 } from '@ui-kitten/components';
-import React from 'react';
+import React, {useContext} from 'react';
 import {RefreshControl, ScrollView} from 'react-native-gesture-handler';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import useSWR from 'swr';
 import {StyleSheet} from 'react-native';
 import {getRoutes} from '../api/routes';
 import {RouterListItem} from '../types';
+import {UserContext} from '../store/user/UserProvider';
 
 type Props = {};
 
 const HomeScreen = (props: Props) => {
   const [refreshing, setRefreshing] = React.useState(false);
+
+  const {currentUser} = useContext(UserContext);
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
@@ -27,7 +30,11 @@ const HomeScreen = (props: Props) => {
     }, 2000);
   }, []);
 
-  const {data: routes, error, isLoading} = useSWR('/routes', getRoutes);
+  const {
+    data: routes,
+  } = useSWR(`/routes?user=${currentUser?.uid}`, () =>
+    getRoutes(currentUser?.uid),
+  );
 
   const renderItem = ({
     item,
